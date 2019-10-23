d33fa6ac2d9e2eb184396fc822b736908df644d3
Jon <jstapleton463@harrisonburg.k12.va.us>
2019-10-23 08:32:01 -0400

    Initial upload

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>This is a Home page</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: 'hello-world',
+  components: {
+    HelloWorld,
+  },
+}
+</script>
